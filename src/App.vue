62dd294d75f5372168e46440f22985db892a6a78
Blade Runner <bladerunner123@sofware-automation.store>
2023-05-03 15:16:15 +0300

    Created TheHeader.vue component

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <template>
+  <the-header />
   <router-view />
 </template>
 
 <script setup lang="ts">
+import TheHeader from "./components/TheHeader.vue";
 </script>
 
 <style lang="scss" scoped>
